4061393d27184bbe94f616616ebe49e69f0cef93
root <root@LAPTOP-QHNNN60G.localdomain>
2022-05-18 20:17:17 +0200

    adding test to all topology functions

--- Testing/Test.h
+++ Testing/Test.h
@@ -4,15 +4,23 @@
 #include "../Topology/Topology.h"
 #include "../JSON/JSON.h"
 
-class Test {
+class Test
+{
 private:
-	Component* _createResistorTestCase(string id, string t1, string t2, double defaultVal, double minVal, double maxVal);
-	Component* _createNMOSTestCase(string id, string drain, string source, string gate, double defaultVal, double minVal, double maxVal);
+	Component *_createResistorTestCase(string id, string t1, string t2, double defaultVal, double minVal, double maxVal);
+	Component *_createNMOSTestCase(string id, string drain, string source, string gate, double defaultVal, double minVal, double maxVal);
 	map<string, double> _createInfo(double defaultVal, double minVal, double MaxVal);
 	UnitTest tester;
 	JSON parser;
+
 public:
 	Test();
+	void testTopologyGetID(Topology *topology, string expectedID);
+	void testTopologyGetComponent(Topology *topology, vector<Component *> expectedComponents);
+	void testTopologyGetComponents(Topology *topology, vector<Component *> expectedComponents);
+	void testTopologyAddComponent(Topology *topology, Component *addedComponent);
+	void testTopologyGetComponentsWithNetlistNode(Topology *topology, string netlistId, vector<Component *> expectedComponents);
+	void testTopologySetComponents(Topology *topology, vector<Component *> components);
 	void testComponent();
 	void testTopology();
 };
